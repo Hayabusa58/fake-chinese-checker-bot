ee679c1eaab83e29491bb5f09490756ef55926a7
Hayabusa58 <hayabusa3430@gmail.com>
2025-01-20 23:10:02 +0900

    文字列全体がチェックされていない問題を修正

--- checker.go
+++ checker.go
@@ -5,12 +5,24 @@ import (
 	"unicode"
 )
 
-func IsKanji(r rune) bool {
-	return unicode.In(r, unicode.Han)
+func IsKanji(rs []rune) bool {
+	flag := true
+	for _, r := range rs {
+		if !(unicode.In(r, unicode.Han)) {
+			flag = false
+		}
+	}
+	return flag
 }
 
-func IsSymbol(r rune) bool {
-	return unicode.IsSymbol(r) || unicode.IsPunct(r)
+func IsSymbol(rs []rune) bool {
+	flag := true
+	for _, r := range rs {
+		if !(unicode.IsSymbol(r) || unicode.IsPunct(r)) {
+			flag = false
+		}
+	}
+	return flag
 }
 
 func IsDiscordMention(s string) bool {
@@ -25,5 +37,5 @@ func IsDiscordEmoji(s string) bool {
 
 func IsAllowedCharacter(s string) bool {
 	r := []rune(s)
-	return IsKanji(r[0]) || IsSymbol(r[0]) || IsDiscordMention(s) || IsDiscordEmoji(s)
+	return IsKanji(r) || IsSymbol(r) || IsDiscordMention(s) || IsDiscordEmoji(s)
 }
